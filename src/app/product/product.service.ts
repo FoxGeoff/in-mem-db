93df2fe222933afde329194ab0c68d6f57b7d38c
Geoff Fox <fox.geoffrey@gmail.com>
2021-02-19 00:19:24 -0800

    Add: error handeling

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,31 +1,62 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { map, filter, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
 import { Product } from '../data/interface/product';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Authorization: 'my-auth-token'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private SERVER_URL: string = "api/products";
+  productesUrl: string = "api/products";
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   public getProducts() {
-    return this.httpClient.get(this.SERVER_URL);
+    return this.http.get(this.productesUrl);
   }
 
   public getProduct(productId) {
-    return this.httpClient.get(`${this.SERVER_URL}/${productId}`);
+    return this.http.get(`${this.productesUrl}/${productId}`);
   }
   public createProduct(product: Product) {
-    return this.httpClient.post(`${this.SERVER_URL}`, product)
+    return this.http.post(`${this.productesUrl}`, product)
   }
 
   public deleteProduct(productId) {
-    return this.httpClient.delete(`${this.SERVER_URL}/${productId}`)
+    return this.http.delete(`${this.productesUrl}/${productId}`)
   }
-  public updateProduct(product: Product) {
-    return this.httpClient.put(`${this.SERVER_URL}/${product.id}`, product)
+
+  /** PUT: update the product on the server. Returns the updated product upon success. */
+  public updateProduct(product: Product): Observable<Product> {
+    return this.http.put<Product>(this.productesUrl, product, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+
 }
